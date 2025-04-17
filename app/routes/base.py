e2a6fee958023435e627fb01a475fc1a4d0ff5b6
KeithAGang <keithgangarahwe@gmail.com>
2025-04-17 20:41:29 +0200

    Added very basic redis caching

--- app/routes/base.py
+++ app/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlmodel import select, Session
 from app.models.users import User, UserCreate
 from app.services.db import get_session
 from uuid6 import uuid7, UUID
+import json
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from .base import APIRouter, HTTPException, Depends, Session, select, uuid7, User, UserCreate, get_session
+from .base import APIRouter, HTTPException, Request, Depends, Session, select, uuid7, User, UserCreate, get_session, json
 from .utils import check_user_exists, check_uuid
 
 usersRouter = APIRouter(
@@ -7,15 +7,26 @@ usersRouter = APIRouter(
 )
 
 @usersRouter.get("/{user_id}")
-def get_users(user_id, session: Session = Depends(get_session)) -> User:
+async def get_users(user_id, request: Request, session: Session = Depends(get_session)) -> User:
     
     if not check_uuid(user_id):
         raise HTTPException(status_code=400, detail="Invalid UUID format")
+    
+    redis = request.app.state.redis
+    redis_key = f"user:{user_id}"
+    
+    #Try fetching From Cache
+    cached_user = await redis.get(redis_key)
+    
+    if cached_user:
+        return User.model_validate_json(cached_user)
     
     user = session.exec(select(User).where(User.id == user_id)).first()
    
     if not user:
        raise HTTPException(status_code=404, detail="User not found")
+   
+    await redis.set(redis_key, user.model_dump_json(), ex=5)
    
     return user
 
