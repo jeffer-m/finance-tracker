39dd04e925b802986cae4009dc12f1a2a40e7c7d
KeithAGang <keithgangarahwe@gmail.com>
2025-04-20 10:24:56 +0200

    Added scalar documentation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 from app.routes.users import usersRouter
 from app.models.users import User
 from contextlib import asynccontextmanager
@@ -26,3 +27,10 @@ app.include_router(usersRouter)
 @app.get("/")
 def default():
     return {"message": "Hello World"}
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
